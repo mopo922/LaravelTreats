1d1e138d9834801f7ee23c1f3478cf6c959ee3db
Mark Potter <markkpotter@gmail.com>
2017-10-17 15:22:03 -0400

    BEGIN V2 - introduce the Repository pattern to API controllers

--- src/Controller/Api/Controller.php
+++ src/Controller/Api/Controller.php
@@ -3,328 +3,127 @@
 namespace LaravelTreats\Controller\Api;
 
 use Auth;
-use Validator;
 use LaravelTreats\Controller\Controller as BaseController;
-use Illuminate\Database\Eloquent\Model;
+use LaravelTreats\Pattern\Repository\Repository;
 use Illuminate\Http\Request;
 
-class Controller extends BaseController
+abstract class Controller extends BaseController
 {
-    /** @var array $errors A pre-validation error description. */
-    protected $errors = [];
-
-    /** @var array $input The user input. */
-    protected $input = [];
-
-    /** @var bool $injectUserId Should we inject a user_id into the input for every request? */
+    /**
+     * Should we inject a user_id into the input for "update" request?
+     *
+     * @var boolean
+     */
     protected $injectUserId = false;
 
-    /** @var bool $returnsArray Does the current action return a data array? */
-    protected $returnsArray = true;
-
-    /** @var string $method The method being called. */
-    protected $method = '';
-
-    /** @var string $model The fully-qualified model class name. */
-    protected $model = '';
-
-    /** @var string $modelNamespace The default model namespace. */
-    protected $modelNamespace = '\App\Model\\';
-
-    /** @var string $redirectUrl Optional custom redirect URL. */
-    protected $redirectUrl = '';
-
-    /** @var string $resourceType The type of resource we're dealing with (User, Campus, etc.) */
-    protected $resourceType = '';
-
-    /** @var string $routePrefix The route name prefix used for the API. */
-    protected $routePrefix = 'api.';
+    /**
+     * Repository data layer. Should be injected in the child class's constructor.
+     *
+     * @var App\Pattern\Repository\Repository $repository
+     */
+    protected $repository;
 
     /**
-     * Extends parent::callAction()
+     * General setup for the whole controller.
      *
-     * Format API responses - JSON for AJAX requests,
-     * redirect back w/ data for normal requests.
-     *
-     * @param string $method
-     * @param array $parameters
-     * @return string|Illuminate\Http\RedirectResponse
+     * @return mixed
      */
-    public function callAction($method, $parameters)
-    {
-        $response = null;
-        $this->method = $method;
-
-        $result = parent::callAction($method, $parameters);
-
-        if ($this->returnsArray) {
-            if (!is_array($result)) {
-                abort(500, 'Bad API response data.');
-            }
-
-            // Return JSON for AJAX and GET requests
-            if (request()->ajax() || 'index' === $method || 'show' === $method) {
-                $response = response()->json($result);
-
-            // Return redirect-back and flash data for normal requests.
-            } else {
-                $response = empty($this->redirectUrl) ? back() : redirect($this->redirectUrl);
-                foreach ($result as $key => $value) {
-                    $response->with($key, $value);
-                }
-                if ('store' !== $method || isset($result['error'])) {
-                    $response->withInput();
-                }
-            }
-        } else {
-            $response = $result;
-        }
-
-        return $response;
-    }
-
-    /** @return mixed General setup for the whole controller. */
     protected function general()
     {
-        $this->resourceType = studly_case(str_replace(
-            $this->routePrefix, '', $this->controller
-        ));
-        if (!$this->model)
-            $this->model = $this->modelNamespace . $this->resourceType;
-        $this->processInput();
-
-        if (!$this->checkPermissions())
+        if (!$this->checkPermissions()) {
             abort(403);
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param int $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        $class = $this->model;
-
-        return $class::destroy($id)
-            ? ['success' => ucfirst($this->readableResourceType()) . ' removed successfully.']
-            : ['error' => 'There was a problem removing the ' . $this->readableResourceType() . '.'];
-    }
-
-    /**
-     * Display a listing of the resource.
-     *
-     * @return array
-     */
-    public function index()
-    {
-        return $this->findModel()->toArray();
-    }
-
-    /**
-     * Use the current input to find a record.
-     *
-     * Apps can override this with their custom methods of finding based on criteria.
-     *
-     * @return Illuminate\Database\Eloquent\Model
-     */
-    protected function findModel()
-    {
-        $class = $this->model;
-        $query = $class::query();
-
-        foreach ($this->input as $key => $value) {
-            $where = is_array($value) ? 'whereIn' : 'where';
-            $query->$where($key, $value);
         }
-
-        return $query->get();
     }
 
     /**
-     * Display the specified resource.
+     * Authorize the current User for this action.
      *
-     * @param int $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        $class = $this->model;
-        return $class::find($id)->toArray();
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @return array
-     */
-    public function store()
-    {
-        $class = $this->model;
-
-        $this->validate(request(), $this->getValidationRules());
-
-        // Create
-        $model = new $class($this->input);
-        return $model->save()
-            ? [
-                'success' => ucfirst($this->readableResourceType()) . ' created successfully.',
-                'model' => $model->toArray(),
-            ]
-            : ['error' => 'There was a problem saving the ' . $this->readableResourceType() . '.'];
-    }
-
-    /**
-     * Get validation rules for the current model.
-     *
-     * Apps can override this with their custom validation rules.
-     *
-     * @return array
-     */
-    protected function getValidationRules()
-    {
-        $class = $this->model;
-
-        return $class::$rules;
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param int $id
-     * @return array
-     */
-    public function update($id)
-    {
-        $return = [];
-        $class = $this->model;
-
-        $this->input['id'] = $id;
-        $this->validate(request(), $this->getValidationRules());
-
-        $model = $class::find($id);
-        if ($this->canEdit($model)) {
-            $this->fillModel($model);
-
-            $return = $model->push()
-                ? ['success' => ucfirst($this->readableResourceType()) . ' updated successfully.']
-                : ['error' => 'There was a problem saving the ' . $this->readableResourceType() . '.'];
-        } else {
-            $return = [
-                'error' => 'You don\'t have permission to edit this '
-                    . $this->readableResourceType() . '.'
-            ];
-        }
-
-        return $return;
-    }
-
-    /**
-     * Check if the current User can edit the given model.
-     *
-     * @param Model $model The model we want to edit.
      * @return bool
      */
-    protected function canEdit(Model $model)
-    {
-        return true;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    protected function buildFailedValidationResponse(Request $request, array $errors)
-    {
-        $response = parent::buildFailedValidationResponse($request, $errors);
-        if (!$response instanceof \Illuminate\Http\JsonResponse) {
-            $response->with('error', 'One or more fields is incorrect.');
-        }
-        return $response;
-    }
-
-    /** @return bool Authorize the current User for this action. */
     protected function checkPermissions()
     {
         return true;
     }
 
     /**
-     * Fill the model with the input.
+     * Display a listing of the resource.
      *
-     * Controllers can override this with custom fill logic.
-     *
-     * @param Illuminate\Database\Eloquent\Model $model
+     * @return \Illuminate\Http\Response
      */
-    protected function fillModel(Model $model)
+    public function index()
     {
-        $model->fill($this->input);
-    }
-
-    /** Retrieve, sanitize, and update the user input. */
-    protected function processInput()
-    {
-        if (empty($this->input)) {
-            $this->input = request()->except('_token');
-
-            if ($this->injectUserId) {
-                $this->input['user_id'] = Auth::user()->id;
-            }
-        }
-        $this->sanitize();
-        request()->replace($this->input); // @todo IS THIS NECESSARY?
-    }
-
-    /** @return string Human-readable version of the resource type. */
-    protected function readableResourceType()
-    {
-        // @TODO EXPLAIN THIS IN THE README
-        return str_replace('_', ' ', snake_case($this->resourceType));
-    }
-
-    /** Sanitize the input - overridden in child class. */
-    protected function sanitize() {}
-
-    /**
-     * Validate the given request with the given rules.
-     *
-     * @param \Illuminate\Http\Request $request
-     * @param array $rules
-     * @param array $messages
-     * @param array $customAttributes
-     */
-    public function validate(Request $request, array $rules, array $messages = [], array $customAttributes = [])
-    {
-        // Check for previous errors
-        if ($this->hasPreValidationErrors()) {
-            $validator = Validator::make([], []);
-            foreach ($this->errors as $field => $error) {
-                $validator->errors()->add($field, $error);
-            }
-            $this->throwValidationException($request, $validator);
-        }
-
-        if ($this->injectUserId && 'index' !== $this->method) {
-            $rules['user_id'] = 'required|in:' . Auth::id();
-        }
-
-        parent::validate($request, $rules, $messages, $customAttributes);
+        return $this->repository->all();
     }
 
     /**
-     * Add a pre-validation error message.
+     * Show the form for creating a new resource.
      *
-     * @param string $field Field name.
-     * @param string $message Error message.
+     * @return \Illuminate\Http\Response
      */
-    protected function addPreValidationError(string $field, string $message)
+    public function create()
     {
-        $this->errors[$field] = $message;
+        //
     }
 
-    /** @return bool Check if we have any pre-validation errors. */
-    protected function hasPreValidationErrors()
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
     {
-        return !empty($this->errors);
+        $data = $request->all();
+
+        if ($this->injectUserId) {
+            $data['user_id'] = Auth::user()->id;
+        }
+
+        return $this->repository->create($data);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return $this->repository->find($id);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        return $this->repository->update($id, $request->all());
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        return $this->repository->delete($id);
     }
 }
---
+++ src/Pattern/Repository/Repository.php
@@ -0,0 +1,281 @@
+<?php
+
+namespace LaravelTreats\Pattern\Repository;
+
+use LaravelTreats\Pattern\Repository\Criteria\_CriteriaInterface;
+use Illuminate\Database\Eloquent\Model;
+use InvalidArgumentException;
+use Validator;
+
+abstract class Repository implements _RepositoryInterface, _HasCriteriaInterface
+{
+    /**
+     * The model instance, for persisting to the data store.
+     *
+     * @var Illuminate\Database\Eloquent\Model $model
+     */
+    protected $model;
+
+    /**
+     * Collection of criteria to apply to the queries.
+     *
+     * @var Illuminate\Support\Collection $criteria
+     */
+    protected $criteria;
+
+    /**
+     * Should the criteria be skipped?
+     *
+     * @var bool $skipCriteria
+     */
+    protected $skipCriteria = false;
+
+    /**
+     * Set up the repository with a model instance.
+     *
+     * @throws InvalidArgumentException
+     */
+    public function __construct()
+    {
+        $this->resetScope();
+        $this->makeModel();
+    }
+
+    /**
+     * Child classes should specify the model class name.
+     *
+     * @return string
+     */
+    abstract protected function modelClass();
+
+    /**
+     * Child classes should specify input validation rules.
+     *
+     * @return array
+     */
+    abstract protected function validationRules();
+
+    /**
+     * Generate an instance of the model.
+     *
+     * @return Illuminate\Database\Eloquent\Model
+     * @throws InvalidArgumentException
+     */
+    public function makeModel()
+    {
+        $model = app()->make($this->modelClass());
+
+        if (!$model instanceof Model) {
+            throw new InvalidArgumentException(
+                'Class ' . $this->model() . ' must be an instance of Illuminate\\Database\\Eloquent\\Model'
+            );
+        }
+
+        return $this->model = $model;
+    }
+
+    /**
+     * Validate and scrub the input data.
+     *
+     * @param array $data Input data from the user
+     * @throws \Illuminate\Validation\ValidationException
+     * @return array Input array, filtered by valid data
+     */
+    protected function validate(array $data)
+    {
+        $validator = Validator::make($data, $this->validationRules());
+        $validator->validate();
+
+        return array_intersect_key($data, $this->validationRules());
+    }
+
+    /**
+     * Get all records of the resource.
+     *
+     * @param array $columns
+     * @return mixed
+     */
+    public function all(array $columns = ['*'])
+    {
+        $this->applyCriteria();
+
+        return $this->model->get($columns);
+    }
+
+    /**
+     * Paginate the resource.
+     *
+     * @param int $perPage
+     * @param array $columns
+     * @return mixed
+     */
+    public function paginate($perPage = 15, $columns = array('*'))
+    {
+        $this->applyCriteria();
+
+        return $this->model->paginate($perPage, $columns);
+    }
+
+    /**
+     * Create a new resource record.
+     *
+     * @param array $data
+     * @return mixed
+     */
+    public function create(array $data)
+    {
+        return $this->model->create($this->validate($data));
+    }
+
+    /**
+     * Update an existing resource record.
+     *
+     * @param mixed $id
+     * @param array $data
+     * @return mixed
+     */
+    public function update($id, array $data)
+    {
+        $model = $this->model->find($id);
+
+        if ($this->canEdit($model)) {
+            $model->fill($this->validate($data));
+            $model->save();
+
+            return $model;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Delete a resource record.
+     *
+     * @param mixed $id
+     * @return mixed
+     */
+    public function delete($id)
+    {
+        return $this->model->destroy($id);
+    }
+
+    /**
+     * Find a specific resource record by ID.
+     *
+     * @param mixed $id
+     * @param array $columns
+     * @return mixed
+     */
+    public function find($id, array $columns = ['*'])
+    {
+        $this->applyCriteria();
+
+        return $this->model->find($id, $columns);
+    }
+
+    /**
+     * Find the first resource record by attribute.
+     *
+     * @param string $attribute
+     * @param mixed $value
+     * @param array $columns
+     * @return mixed
+     */
+    public function findBy(string $attribute, $value, array $columns = ['*'])
+    {
+        $this->applyCriteria();
+
+        return $this->model->where($attribute, '=', $value)->first($columns);
+    }
+
+    /**
+     * Set all criteria settings back to the default.
+     *
+     * @return $this
+     */
+    public function resetScope()
+    {
+        $this->criteria = collect([]);
+        $this->skipCriteria(false);
+
+        return $this;
+    }
+
+    /**
+     * Set a flag to skip all collected criteria.
+     *
+     * @param bool $status Should criteria be skipped?
+     * @return $this
+     */
+    public function skipCriteria($status = true)
+    {
+        $this->skipCriteria = $status;
+
+        return $this;
+    }
+
+    /**
+     * Get the current list of criteria objects.
+     *
+     * @return Illuminate\Support\Collection
+     */
+    public function getCriteria()
+    {
+        return $this->criteria;
+    }
+
+    /**
+     * Apply a single criteria to the model.
+     *
+     * @param _CriteriaInterface $criteria
+     * @return $this
+     */
+    public function getByCriteria(_CriteriaInterface $criteria)
+    {
+        $this->model = $criteria->apply($this->model, $this);
+
+        return $this;
+    }
+
+    /**
+     * Add a criteria to the collection.
+     *
+     * @param _CriteriaInterface $criteria
+     * @return $this
+     */
+    public function pushCriteria(_CriteriaInterface $criteria)
+    {
+        $this->criteria->push($criteria);
+
+        return $this;
+    }
+
+    /**
+     * Apply all collected criteria to the model.
+     *
+     * @return $this
+     */
+    public function applyCriteria()
+    {
+        if (!$this->skipCriteria) {
+            foreach ($this->getCriteria() as $criteria) {
+                if ($criteria instanceof _CriteriaInterface) {
+                    $this->model = $criteria->apply($this->model, $this);
+                }
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * Check if the current User can edit the given model.
+     *
+     * @param Model $model The model we want to edit.
+     * @return bool
+     */
+    protected function canEdit(Model $model)
+    {
+        return true;
+    }
+ }
