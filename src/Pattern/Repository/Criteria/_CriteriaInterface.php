3b10a1544f02e3791a4d495b871aea76ba184b25
Mark Potter <markkpotter@gmail.com>
2017-10-20 00:09:40 -0400

    fix repository criteria bug

--- src/Pattern/Repository/Criteria/_CriteriaInterface.php
+++ src/Pattern/Repository/Criteria/_CriteriaInterface.php
@@ -2,15 +2,13 @@
 
 namespace LaravelTreats\Pattern\Repository\Criteria;
 
-use Illuminate\Database\Eloquent\Model;
-
 interface _CriteriaInterface
 {
     /**
      * Apply the criteria to a model.
      *
-     * @param Illuminate\Database\Eloquent\Model $model Eloquent model to which the criteria are applied.
+     * @param Illuminate\Database\Eloquent\Model|Illuminate\Database\Eloquent\Builder $model Eloquent model to which the criteria are applied.
      * @return mixed
      */
-    public function apply(Model $model);
+    public function apply($model);
 }
